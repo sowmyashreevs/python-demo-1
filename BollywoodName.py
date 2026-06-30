ea279fe3b64df2461a4276285492f4e4543675ca
sowmyashreevs <67633889+sowmyashreevs@users.noreply.github.com>
2026-06-30 12:06:31 +0530

    Update BollywoodName.py

--- BollywoodName.py
+++ BollywoodName.py
@@ -1,1 +1,10 @@
-
+#Bollywood Name Generator:
+first_name = input("Enter the first name of any bollywood actor")
+last_name = input("Enter the last name of any bollywood actor")
+print(f'*************************************************************************\n')
+print(f'***   Entered Name: "{first_name} {last_name}"')
+print(f'***   Hero:         "{first_name[::-1]} {last_name.upper()}"')
+print(f'***   Villain:      "THE {first_name.upper()} {last_name.upper()}"')
+print(f'***   Comedian:     "{first_name.title()} {last_name.title()} Ji"')
+print();
+print(f'*************************************************************************\n')
